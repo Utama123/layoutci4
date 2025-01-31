6476c9ae74cb7e0081a52903e7df6fe5a2153bed
Pramanca <pramancautama@gmail.com>
2025-01-31 11:21:15 +0700

    mengedit file about dengan section

--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?=$title?></title>
-</head>
-<body>
-    <h1>Ini adalah about</h1>
-</body>
-</html>
+<?=  $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<h1>Hello, ini adalah about!</h1>
+<?= $this->endSection('content'); ?>
+    
